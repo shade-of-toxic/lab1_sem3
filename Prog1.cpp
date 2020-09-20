ecfe72010fe72569a574b5d338570dc930deb238
Елизавета Казакова <lisa050701@bk.ru>
2020-09-20 14:42:01 +0300

    Fix

--- Prog1.cpp
+++ Prog1.cpp
@@ -2,10 +2,9 @@
 #include <algorithm>
 
 namespace Prog1 {
-	double** input(int& rm, int& rn, std::istream &stream = std::cin)
-	{
+
+	Matrix input(std::istream& stream = std::cin) {
 		const char* pr = ""; // будущее сообщение об ошибке
-		double** lines = nullptr;  // динамический массив строк матрицы
 		int m;	// количество строк матрицы
 		int n;
 		// сначала вводим количество строк
@@ -14,82 +13,59 @@ namespace Prog1 {
 			std::cout << "Enter number of lines: --> ";
 			pr = "You are wrong; repeat please!";
 			if (getNum(m, stream) < 0) // обнаружена ошибка ввода или конец файла
-				return nullptr;
+				return Matrix(0, 0);
 		} while (m < 1);
-
-		// выделяем память под массив структур - строк матрицы
-		try {
-			lines = new double*[m];
-		}
-		catch (std::bad_alloc & ba)
-		{
-			std::cout << "------ too many rows in matrix: " << ba.what() << std::endl;
-			return nullptr;
-		}
-		pr = "";
 		do {
 			std::cout << pr << std::endl;
-			std::cout << "Enter number of items in any line --> ";
+			std::cout << "Enter number of columns: --> ";
 			pr = "You are wrong; repeat please!";
-			if (getNum(n, stream) < 0) { // обнаружена ошибка ввода или конец файла
-				return nullptr;
-			}
+			if (getNum(n, stream) < 0) // обнаружена ошибка ввода или конец файла
+				return Matrix(0, 0);
 		} while (n < 1);
-		for (int i = 0; i < m; i++) {
-			// теперь для каждой строки матрицы вводим количество столбцов 
-			
-			// и выделяем необходимую память под элементы строки
-			try {
-				lines[i] = new double[n];
-			}
-			catch (std::bad_alloc & ba)
-			{
-				std::cout << "------ too many items in matrix: " << ba.what() << std::endl;
-				erase(lines, i);
-				return nullptr;
-			}
+		Matrix mat(m, n);
 
+		for (int i = 0; i < m; i++) {
 			// теперь можно вводить сами элементы данной строки матрицы
 			std::cout << "Enter items for matrix line #" << (i + 1) << ":" << std::endl;
 			for (int j = 0; j < n; j++) {
-				if (getNum(lines[i][j], stream) < 0) { // обнаружена ошибка ввода или конец файла
-					erase(lines, i + 1); // освобождение памяти, занятой ранее введенными строками
-					return nullptr;
+				double elem;
+				if (getNum(elem, stream) < 0) { // обнаружена ошибка ввода или конец файла
+					return Matrix(0, 0);
 				}
+				if (elem != 0)
+					mat.elems.push_back({i, j, elem});
 			}
 		}
-		// формируем результат - количество строк в матрице
-		rm = m;
-		rn = n;
-		return lines;
+		return mat;
 	}
 
-	void output(const char* msg, double** a, int const& m, int const& n)
-	{
-		int i, j;
-		std::cout << msg << ":" << std::endl;
-		for (i = 0; i < m; ++i) {
-			for (j = 0; j < n; ++j)
-				std::cout << a[i][j] << "  ";
-			std::cout << std::endl;
+	void Matrix::output() {
+		auto elem = elems.begin();
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (elem != elems.end() && elem->x == i && elem->y == j) {
+					std::cout << elem->val << "  ";
+					elem++;
+				}
+				else
+					std::cout << "0  ";
+			}
+			std::cout << "\n";
 		}
+		std::cout.flush();// сброс буфера
 	}
 
-	// функция освобождения занятой памяти
-	void erase(double**& lines, int const& m)
-	{
-		int i;
-		for (i = 0; i < m; i++)
-			delete[] lines[i];
-		delete[] lines;
-	}
+	Matrix swapij(Matrix const& mat) {
+		auto prev = mat.elems.begin(), next = mat.elems.end();
 
+	}
+	
 	double** swapij(double**& lines, int const& m, int const& n) {
 		double** newmatrix = new double* [m];
 		for (int i = 0; i < m; i++) {
 			newmatrix[i] = new double[n];
 			int a = -1, b = -1;
-			for (int j = 0; j < (n-1); j++) {
+			for (int j = 0; j < (n - 1); j++) {
 				if (a < 0 && lines[i][j] < lines[i][j + 1])
 					a = j + 1;
 				if (lines[i][j] > lines[i][j + 1])
@@ -103,4 +79,87 @@ namespace Prog1 {
 		}
 		return newmatrix;
 	}
+
+	// double** input(int& rm, int& rn, std::istream &stream = std::cin)
+	// {
+	// 	const char* pr = ""; // будущее сообщение об ошибке
+	// 	double** lines = nullptr;  // динамический массив строк матрицы
+	// 	int m;	// количество строк матрицы
+	// 	int n;
+	// 	// сначала вводим количество строк
+	// 	do {
+	// 		std::cout << pr << std::endl;
+	// 		std::cout << "Enter number of lines: --> ";
+	// 		pr = "You are wrong; repeat please!";
+	// 		if (getNum(m, stream) < 0) // обнаружена ошибка ввода или конец файла
+	// 			return nullptr;
+	// 	} while (m < 1);
+
+	// 	// выделяем память под массив структур - строк матрицы
+	// 	try {
+	// 		lines = new double*[m];
+	// 	}
+	// 	catch (std::bad_alloc & ba)
+	// 	{
+	// 		std::cout << "------ too many rows in matrix: " << ba.what() << std::endl;
+	// 		return nullptr;
+	// 	}
+	// 	pr = "";
+	// 	do {
+	// 		std::cout << pr << std::endl;
+	// 		std::cout << "Enter number of items in any line --> ";
+	// 		pr = "You are wrong; repeat please!";
+	// 		if (getNum(n, stream) < 0) { // обнаружена ошибка ввода или конец файла
+	// 			return nullptr;
+	// 		}
+	// 	} while (n < 1);
+	// 	for (int i = 0; i < m; i++) {
+	// 		// теперь для каждой строки матрицы вводим количество столбцов 
+
+	// 		// и выделяем необходимую память под элементы строки
+	// 		try {
+	// 			lines[i] = new double[n];
+	// 		}
+	// 		catch (std::bad_alloc & ba)
+	// 		{
+	// 			std::cout << "------ too many items in matrix: " << ba.what() << std::endl;
+	// 			erase(lines, i);
+	// 			return nullptr;
+	// 		}
+
+	// 		// теперь можно вводить сами элементы данной строки матрицы
+	// 		std::cout << "Enter items for matrix line #" << (i + 1) << ":" << std::endl;
+	// 		for (int j = 0; j < n; j++) {
+	// 			if (getNum(lines[i][j], stream) < 0) { // обнаружена ошибка ввода или конец файла
+	// 				erase(lines, i + 1); // освобождение памяти, занятой ранее введенными строками
+	// 				return nullptr;
+	// 			}
+	// 		}
+	// 	}
+	// 	// формируем результат - количество строк в матрице
+	// 	rm = m;
+	// 	rn = n;
+	// 	return lines;
+	// }
+
+	// void output(const char* msg, double** a, int const& m, int const& n)
+	// {
+	// 	int i, j;
+	// 	std::cout << msg << ":" << std::endl;
+	// 	for (i = 0; i < m; ++i) {
+	// 		for (j = 0; j < n; ++j)
+	// 			std::cout << a[i][j] << "  ";
+	// 		std::cout << std::endl;
+	// 	}
+	// }
+
+	// // функция освобождения занятой памяти
+	// void erase(double**& lines, int const& m)
+	// {
+	// 	int i;
+	// 	for (i = 0; i < m; i++)
+	// 		delete[] lines[i];
+	// 	delete[] lines;
+	// }
+
 }
--- Prog1.hpp
+++ Prog1.hpp
@@ -1,17 +1,30 @@
 #include <iostream>
+#include <list>
 
 namespace Prog1 {
 
 	template <class T>
-	int getNum(T& a, std::istream &stream)
+	int getNum(T& a, std::istream& stream)
 	{
 		stream >> a;
 		if (!stream.good())	// обнаружена ошибка ввода или конец файла
 			return -1;
 		return 1;
 	}
-	double** input(int& rm, int& rn, std::istream &stream = std::cin);
-	void output(const char* msg, double** a, int const& m, int const& n);
-	void erase(double**& lines, int const& m);
-	double** swapij(double**& lines, int const& m, int const& n);
+
+	struct MatItem {
+		int x, y;
+		double val;
+	};
+
+	struct Matrix {
+		int n, m;
+		std::list<MatItem> elems;
+
+		Matrix(int const& _m, int const& _n) : n(_n), m(_m) {}
+		void output();
+	};
+
+	Matrix input(std::istream& stream = std::cin);
+	Matrix swapij(Matrix const& mat);
 }
